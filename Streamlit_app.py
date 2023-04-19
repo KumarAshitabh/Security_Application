5481b5f29ff9a8cee33de27d5e712effed071f02
ashitabh <ashitabh@yahoo.com>
2023-04-19 12:38:11 +0530

    .

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,40 +2,32 @@ import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from streamlit_echarts import st_echarts 
+from st_aggrid import AgGrid, GridUpdateMode, DataReturnMode, JsCode
 st.set_page_config(page_title="Security & Applications", layout="wide") 
 st.title("Dashboard - Credit Card Transactions")
 
 df = pd.read_csv("./data/sample.csv")
 df.index.rename('Serial No', inplace=True)
-
-df = pd.DataFrame({
-    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-    'Age': [25, 30, 35, 40],
-    'Score': [80, 90, 70, 85]
-})
-
-
-def score_style(val):
-    '''
-    highlight the cell if the value is greater than or equal to 80
-    '''
-    if val >= 80:
-        return 'background-color: yellow'
-    else:
-        return ''
-
-
-
+# add this
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=20)
 
+cellstyle_jscode = streamlit_echarts.JsCode("""
+function(params){
+    if (params.value == '1') {
+        return {
+            'color': 'black', 
+            'backgroundColor': 'orange',
+        }
+    }    
+}
+""")
+
+
+gb.configure_pagination(enabled=True)
+gb.configure_columns(df, cellStyle= cellstyle_jscode)
 gb.configure_side_bar()
 gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
 gridOptions = gb.build()
 
-AgGrid(df, columnDefs=[
-              {'headerName': 'trans_date_trans_time', 'field': 'trans_date_trans_time'},
-              {'headerName': 'merch_long', 'field': 'merch_long'},
-              {'headerName': 'is_fraud', 'field': 'is_fraud', 'cellStyle': score_style}
-          ],allow_unsafe_jscode=True)
+AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True)
