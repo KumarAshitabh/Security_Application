5a31e7c0f512bbae2fae0bf2117c53d9b2094e93
ashitabh <ashitabh@yahoo.com>
2023-04-19 13:09:43 +0530

    .

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -33,7 +33,7 @@ gb.configure_side_bar()
 gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
 gridOptions = gb.build()
 
-AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True, allow_unsafe_jscode=True)
+#AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True, allow_unsafe_jscode=True)
 
 
 
