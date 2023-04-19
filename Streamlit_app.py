18ef0943326fd11a121edeef93506e4fbc6bbd44
ashitabh <ashitabh@yahoo.com>
2023-04-19 12:49:13 +0530

    .

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,10 +3,9 @@ import streamlit as st
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid import AgGrid, GridUpdateMode, DataReturnMode, JsCode
-from streamlit import caching
+
 
 # Allow unsafe JavaScript code
-caching.clear_cache()
 st.set_page_config(allow_unsafe_jscode=True)
 st.set_page_config(page_title="Security & Applications", layout="wide") 
 st.title("Dashboard - Credit Card Transactions")
