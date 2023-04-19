379a3ad7abf38dc2a9203d8ae05a0d172e9ce791
ashitabh <ashitabh@yahoo.com>
2023-04-19 12:56:36 +0530

    ...

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,8 +5,7 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid import AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 
-# Allow unsafe JavaScript code
-st.set_page_config(allow_unsafe_jscode=True)
+
 st.set_page_config(page_title="Security & Applications", layout="wide") 
 st.title("Dashboard - Credit Card Transactions")
 
@@ -34,4 +33,4 @@ gb.configure_side_bar()
 gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
 gridOptions = gb.build()
 
-AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True)
+AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True, allow_unsafe_jscode=True)
