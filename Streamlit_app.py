f0e7ba2d91f9945d8763640e4918f756bf1f1c54
ashitabh <ashitabh@yahoo.com>
2023-04-20 11:58:43 +0530

    .

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,28 +3,38 @@ import streamlit as st
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid import AgGrid, GridUpdateMode, DataReturnMode, JsCode
-
-
+from confluent_kafka import Consumer, OFFSET_BEGINNING
+from confluent_kafka import Consumer, KafkaError 
+from io import BytesIO
 
 st.set_page_config(page_title="Security & Applications", layout="wide") 
 st.title("Dashboard - Credit Card Transactions")
 
-df = pd.read_csv("./data/sample.csv").head(5)
+conf = {
+        'bootstrap.servers': st.secrets["default"]["bootstrap.servers"],
+        'sasl.username': st.secrets["default"]["sasl.username"],
+        'sasl.password': st.secrets["default"]["sasl.password"],
+        'security.protocol': st.secrets["default"]["security.protocol"],
+        'sasl.mechanisms': st.secrets["default"]["sasl.mechanisms"],
+        'group.id': st.secrets["consumer"]["group.id"],
+        'auto.offset.reset': st.secrets["consumer"]["auto.offset.reset"]
+    }
 
-# add this
+df = pd.read_csv("./data/sample.csv")
+
 gb = GridOptionsBuilder.from_dataframe(df)
 gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=20)
 
 cellstyle_jscode = JsCode("""
-function(params){
-    if (params.data.is_fraud == '1') {
-        return {
-            'color': 'black', 
-            'backgroundColor': 'Red',
+        function(params){
+            if (params.data.is_fraud == '1') {
+                return {
+                    'color': 'black', 
+                    'backgroundColor': 'Red',
+                }
+            }    
         }
-    }    
-}
-""")
+        """)
 
 
 
@@ -34,16 +44,38 @@ gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, agg
 gridOptions = gb.build()
 gridOptions['getRowStyle'] = cellstyle_jscode
 
-grid_result = AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True, allow_unsafe_jscode=True,reload_data=True,key='trans_num')
 
+def dashboard():
+    grid_result = AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True, allow_unsafe_jscode=True,reload_data=True )
 
-# add a button to refresh the AgGrid
+if __name__ == '__main__':
+    dashboard()
+
+consumer = Consumer(conf)
+topic = 'topic_0'
+consumer.subscribe([topic])
+
 if st.button('Refresh'):
-    df = pd.read_csv("./data/sample.csv")
-    df = df.head(20)
-    df.to_csv('./data/sample.csv', index=False)
     st.experimental_rerun()
 
-# display the result
-st.write('Selected:', grid_result['selected_rows'])
+
+while True:
+    msg = consumer.poll(10.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            print('End of partition reached')
+        else:
+            print('Error while consuming message: {}'.format(msg.error()))
+    else:
+        print('Received message:  value={} , offset={}'.format(  msg.value(),   msg.offset()))
+        #df1 = pd.read_json(BytesIO(msg.value()), orient="records")
+        df = df.append(pd.read_json(BytesIO(msg.value()), orient="records"))
+        print(df)
+        df.to_csv('./data/sample.csv', index=False)
+        grid_result = AgGrid(df, gridOptions=gridOptions, enable_enterprise_modules=True, allow_unsafe_jscode=True,reload_data=True )
+
+
  
